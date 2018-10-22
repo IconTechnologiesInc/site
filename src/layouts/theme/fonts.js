ef0221a67edc0f9a5d97fd4fa644a37cfcea5816
Brandon <brand.fleming@gmail.com>
2018-10-22 09:01:57 -0400

    #1 Created new nav header and fixed mobile

---
+++ src/layouts/theme/fonts.js
@@ -0,0 +1,11 @@
+export default {
+    size: {
+        largeest: '6rem',
+        largeer: '3rem',
+        large: '2rem',
+        medium: '1.5rem',
+        small: '1.2rem',
+        smaller: '1rem',
+        smallest: '0.75rem'
+    }
+}
